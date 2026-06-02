e9155351fe90bb3413a6e1baf082ef852dced894
Ben Simmons <ben@Bens-MacBook-Air-2.local>
2026-06-02 13:06:16 -0500

    Register lexxy stylesheet path in host app's asset pipeline

--- lib/plum/engine.rb
+++ lib/plum/engine.rb
@@ -5,5 +5,12 @@ module Plum
     isolate_namespace Plum
     config.paths["config/routes.rb"] = "config/plum_routes.rb"
     config.paths["db/migrate"] = []
+
+    initializer "plum.assets" do |app|
+      if defined?(Lexxy::Engine)
+        lexxy_stylesheets = Lexxy::Engine.root.join("app/assets/stylesheets")
+        app.config.assets.paths << lexxy_stylesheets.to_s if lexxy_stylesheets.exist?
+      end
+    end
   end
 end
