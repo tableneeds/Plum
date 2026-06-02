f93f1f93b83c101b2d3aa4b96ee742b377aabb29
Ben Simmons <ben@Bens-MacBook-Air-2.local>
2026-06-02 13:08:29 -0500

    Use gem spec path for lexxy assets instead of Lexxy::Engine

--- lib/plum/engine.rb
+++ lib/plum/engine.rb
@@ -7,9 +7,13 @@ module Plum
     config.paths["db/migrate"] = []
 
     initializer "plum.assets" do |app|
-      if defined?(Lexxy::Engine)
-        lexxy_stylesheets = Lexxy::Engine.root.join("app/assets/stylesheets")
-        app.config.assets.paths << lexxy_stylesheets.to_s if lexxy_stylesheets.exist?
+      lexxy_spec = Gem.loaded_specs["lexxy"]
+      if lexxy_spec
+        lexxy_root = Pathname.new(lexxy_spec.gem_dir)
+        %w[app/assets/stylesheets app/javascript].each do |subpath|
+          path = lexxy_root.join(subpath)
+          app.config.assets.paths << path.to_s if path.exist?
+        end
       end
     end
   end
